44484225e3f67816c1eaf08b66ad4546f8ab0834
Daniel Santana <github@danielsantana.net>
2018-03-27 18:42:24 -0400

    adds realClone method

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const clone = o => JSON.parse(JSON.stringify(o));
 
-module.exports = { clone };
+const realClone = (obj, history = []) => {
+  if (history.includes(obj)) return '[Circular]';
+
+  if (Array.isArray(obj)) {
+    history.push(obj);
+    return obj.map(_ => realClone(_, history));
+  }
+
+  if (typeof obj === 'object') {
+    history.push(obj);
+    const ret = {};
+
+    Object.keys(obj).forEach((key) => {
+      ret[key] = realClone(obj[key], history);
+    });
+
+    return ret;
+  }
+  return obj;
+};
+
+module.exports = { clone, realClone };
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 
 const { expect } = require('chai');
-const { clone }  = require('../');
+const { clone, realClone }  = require('../');
 
 describe('clone', () => {
   it('should return a different obj with the same structure', () => {
@@ -9,5 +9,39 @@ describe('clone', () => {
 
     expect(result).to.not.equal(original);
     expect(result).to.deep.equal(original);
+  });
+});
+
+describe('realClone', () => {
+  it('should return a different obj with the same structure', () => {
+    const original = {a: 1, b: 'two', c:[true], d:{e:'f'}};
+    const result = realClone(original);
+
+    expect(result).to.not.equal(original);
+    expect(result).to.deep.equal(original);
+  });
+
+  it('should return a different Array with the copy of the values', () => {
+    const original = [{a: 1}, {b: 'two'}, [true], {d:{e:'f'}}];
+    const result = realClone(original);
+
+    expect(result).to.not.equal(original);
+    expect(result).to.deep.equal(original);
+  });
+
+  it('should return a clone of the input signalizing the Circular ref', () => {
+    const someObject = { a: 1, original: '[Circular]' };
+    const original = [someObject, {b: 'two'}, [true], {d:{e:'f'}}];
+
+    // cloning original before adding Circular ref
+    const expected = clone(original);
+
+    // adds circular ref
+    someObject.original = original;
+
+    const result = realClone(original);
+
+    expect(result).to.not.equal(original);
+    expect(result).to.deep.equal(expected);
   });
 });
