9ae4107e64bd4ecfb1bd32603029805505848c3c
Daniel Santana <github@danielsantana.net>
2019-05-28 14:43:14 -0400

    standardize linting with standard.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const clone = o => JSON.parse(JSON.stringify(o));
+const clone = o => JSON.parse(JSON.stringify(o))
 
 const realClone = (obj, history = []) => {
-  if (history.includes(obj)) return '[Circular]';
+  if (history.includes(obj)) return '[Circular]'
 
   if (Array.isArray(obj)) {
-    history.push(obj);
-    return obj.map(_ => realClone(_, history));
+    history.push(obj)
+    return obj.map(_ => realClone(_, history))
   }
 
   if (typeof obj === 'object') {
-    history.push(obj);
-    const ret = {};
+    history.push(obj)
+    const ret = {}
 
     Object.keys(obj).forEach((key) => {
-      ret[key] = realClone(obj[key], history);
-    });
+      ret[key] = realClone(obj[key], history)
+    })
 
-    return ret;
+    return ret
   }
-  return obj;
-};
+  return obj
+}
 
-module.exports = { clone, realClone };
+module.exports = { clone, realClone }
--- test/index.test.js
+++ test/index.test.js
@@ -1,47 +1,47 @@
 
-const { expect } = require('chai');
-const { clone, realClone }  = require('../');
+const { expect } = require('chai')
+const { clone, realClone } = require('../')
 
 describe('clone', () => {
   it('should return a different obj with the same structure', () => {
-    const original = {a: 1, b: 'two', c:[true], d:{e:'f'}};
-    const result = clone(original);
+    const original = { a: 1, b: 'two', c: [true], d: { e: 'f' } }
+    const result = clone(original)
 
-    expect(result).to.not.equal(original);
-    expect(result).to.deep.equal(original);
-  });
-});
+    expect(result).to.not.equal(original)
+    expect(result).to.deep.equal(original)
+  })
+})
 
 describe('realClone', () => {
   it('should return a different obj with the same structure', () => {
-    const original = {a: 1, b: 'two', c:[true], d:{e:'f'}};
-    const result = realClone(original);
+    const original = { a: 1, b: 'two', c: [true], d: { e: 'f' } }
+    const result = realClone(original)
 
-    expect(result).to.not.equal(original);
-    expect(result).to.deep.equal(original);
-  });
+    expect(result).to.not.equal(original)
+    expect(result).to.deep.equal(original)
+  })
 
   it('should return a different Array with the copy of the values', () => {
-    const original = [{a: 1}, {b: 'two'}, [true], {d:{e:'f'}}];
-    const result = realClone(original);
+    const original = [{ a: 1 }, { b: 'two' }, [true], { d: { e: 'f' } }]
+    const result = realClone(original)
 
-    expect(result).to.not.equal(original);
-    expect(result).to.deep.equal(original);
-  });
+    expect(result).to.not.equal(original)
+    expect(result).to.deep.equal(original)
+  })
 
   it('should return a clone of the input signalizing the Circular ref', () => {
-    const someObject = { a: 1, original: '[Circular]' };
-    const original = [someObject, {b: 'two'}, [true], {d:{e:'f'}}];
+    const someObject = { a: 1, original: '[Circular]' }
+    const original = [someObject, { b: 'two' }, [true], { d: { e: 'f' } }]
 
     // cloning original before adding Circular ref
-    const expected = clone(original);
+    const expected = clone(original)
 
     // adds circular ref
-    someObject.original = original;
+    someObject.original = original
 
-    const result = realClone(original);
+    const result = realClone(original)
 
-    expect(result).to.not.equal(original);
-    expect(result).to.deep.equal(expected);
-  });
-});
+    expect(result).to.not.equal(original)
+    expect(result).to.deep.equal(expected)
+  })
+})
